2be1cc61b31673b475c0e4a4d8aa3c9645f3c486
shukrullojonappdev <shukrullojon.dev@gmail.com>
2023-12-07 12:08:23 +0500

    commited

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,9 +2,16 @@
   <q-layout view="hHh lpR fFf">
     <q-header>
       <q-toolbar>
-        <q-btn v-if="backButtonVisible" flat round dense icon="arrow_back" />
+        <q-btn
+          v-if="backButtonVisible"
+          flat
+          round
+          dense
+          icon="arrow_back"
+          @click="goBack()"
+        />
         <q-toolbar-title class="toobar-title">{{ route.name }}</q-toolbar-title>
-        <q-btn v-if="seatchButtonVisible" flat round dense icon="search" />
+        <q-btn v-if="searchButtonVisible" flat round dense icon="search" />
       </q-toolbar>
     </q-header>
 
@@ -12,7 +19,7 @@
       <router-view />
     </q-page-container>
 
-    <q-footer>
+    <q-footer :model-value="footerVisible">
       <q-tabs>
         <q-route-tab to="/" name="mails" icon="message" />
         <q-route-tab to="/explore" name="alarms" icon="explore" />
@@ -24,20 +31,29 @@
 
 <script setup lang="ts">
 import { ref, watch } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
-const seatchButtonVisible = ref(true);
+const router = useRouter();
+
+const searchButtonVisible = ref(true);
 const backButtonVisible = ref(false);
+const footerVisible = ref(true);
 
 watch(route, () => {
   route.name === "chats"
-    ? (seatchButtonVisible.value = true)
-    : (seatchButtonVisible.value = false);
+    ? (searchButtonVisible.value = true)
+    : ((searchButtonVisible.value = false), (footerVisible.value = true));
   route.name === "chat"
-    ? (backButtonVisible.value = true)
+    ? ((backButtonVisible.value = true), (footerVisible.value = false))
     : (backButtonVisible.value = false);
 });
+
+function goBack() {
+  console.log("asdf");
+
+  router.go(-1);
+}
 </script>
 
 <style scoped lang="scss"></style>
--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,5 +1,64 @@
 <template>
-  <q-page> asdf </q-page>
+  <q-page>
+    <div class="container">
+      <div class="messages">
+        <q-virtual-scroll
+          ref="messages"
+          scroll-target=".messages"
+          :items="heavyList"
+          separator
+          v-slot="{ item, index }"
+        >
+          <q-chat-message
+            :key="index"
+            name="me"
+            avatar="https://cdn.quasar.dev/img/avatar1.jpg"
+            :text="['hey, how are you?', index]"
+            sent
+          />
+        </q-virtual-scroll>
+      </div>
+      <q-input class="input" ref="input" v-model="text" filled autogrow />
+    </div>
+  </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { ref } from "vue";
+
+const text = ref("");
+const messages: any = ref(null);
+const input: any = ref(null);
+
+const maxSize = 10000;
+const heavyList: any = [];
+
+for (let i = 0; i < maxSize; i++) {
+  heavyList.push({
+    label: "Option " + (i + 1),
+  });
+}
+
+onMounted(() => {
+  messages.value.scrollTo(heavyList.length - 2);
+  console.log(input.value);
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+  position: relative;
+}
+.messages {
+  position: absolute;
+  top: 0;
+  bottom: 100px;
+  overflow: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+.messages::-webkit-scrollbar {
+  display: none;
+}
+</style>
